bc63489b43f9ae98d36ef2db678a9b56445d0b5b
Asmita Borawake <asmita.borawake@Sneha-Sures-Mac.local>
2019-10-04 16:45:05 +0530

    demo of realm with create, update, delete data

---
+++ RealmCRUDDemo/PickUpLineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PickUpLineTableViewCell.swift
+//  RealmCRUDDemo
+//
+//  Created by Asmita Borawake on 04/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import UIKit
+
+class PickUpLineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RealmCRUDDemo/RealmService.swift
@@ -0,0 +1,9 @@
+//
+//  RealmService.swift
+//  RealmCRUDDemo
+//
+//  Created by Asmita Borawake on 04/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import Foundation
